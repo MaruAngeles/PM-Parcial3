4965745ab0c6a9778e9384c3fa74ddfdc427a416
MaruAngeles <marurufi999@gmail.com>
2026-07-28 01:03:06 -0600

    Práctica 20

--- usuarioApi/app/(tabs)/_layout.js
+++ usuarioApi/app/(tabs)/_layout.js
@@ -1,22 +1,53 @@
 //importar la navegacion que queremos que tenga
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
-export default function TabsLayout(){
-    
-    return(
 
-        <Tabs>
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
 
-        {/* // nombre que aparece debajo de la navegacion */}
+        tabBarActiveTintColor: "#db2f2f",
+        tabBarInactiveTintColor: "#6B7280",
 
-        <Tabs.Screen name="index" options={{title:"Inicio", href:null, tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />),}}/>
-        <Tabs.Screen name="Alta" options={{title:"Alta", tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-add" size={size} color={color} />),}}/>
-        <Tabs.Screen name="Consulta" options={{title:"consulta", tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search" size={size} color={color} />),}}/>
+        tabBarStyle: {
+          height: 60,
+          backgroundColor: "#fff",
+        },
 
-        </Tabs>
+        tabBarLabelStyle: {
+          fontSize: 13,
+          marginBottom: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+            href: null,
+        }}
+      />
 
-    );
+      <Tabs.Screen
+        name="alta"
+        options={{
+          title: "alta",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-add-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="consulta"
+        options={{
+          title: "consulta",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
